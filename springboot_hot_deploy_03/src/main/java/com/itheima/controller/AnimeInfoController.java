5d3df4a522b3a4be5dc072c197856c0df022c41c
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-21 08:07:25 +0800

    AM

---
+++ springboot_hot_deploy_03/src/main/java/com/itheima/controller/AnimeInfoController.java
@@ -0,0 +1,31 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_second
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-21 07:14:39
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@RequestMapping("/animeInfo")
+@RestController
+public class AnimeInfoController {
+
+    @GetMapping
+    public String getString01(){
+
+        System.out.println("sayHello...");
+        System.out.println("sayHello...");
+        String format = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss").format(LocalDateTime.now());
+        return "sayHello...";
+
+    }
+}
