50889e4db3f7f0a884374c505590f88d23ccd2cb
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-23 07:21:34 +0800

    AM

--- springboot_transactional_01/src/test/java/com/itheima/SpringbootTransactional01ApplicationTests.java
+++ springboot_transactional_01/src/test/java/com/itheima/SpringbootTransactional01ApplicationTests.java
@@ -22,8 +22,10 @@ class SpringbootTransactional01ApplicationTests {
         AnimeInfo animeInfo = new AnimeInfo();
         animeInfo.setName("在地下城邂逅是否搞错了什么...");
         animeInfo.setReleaseTime(2015);
-        animeInfo.setCharacter01("贝尔克朗尼松冈祯丞");
-        animeInfo.setCharacter02("希尔");
+        animeInfo.setCharacter01("贝尔克朗尼松冈祯丞02");
+        int i = 10/0;
+        animeInfo.setCharacter02("希尔石上静香02");
+
         animeInfo.setId(33);
 //        Integer integer = animeInfoDao.insertOne(animeInfo);
         Integer integer = animeInfoDao.updateById(animeInfo);
