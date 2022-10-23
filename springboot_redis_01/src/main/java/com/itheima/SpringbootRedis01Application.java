0d5e49ed5300e2246454c0754b40abc087692ed3
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-23 08:06:35 +0800

    AM

--- springboot_redis_01/src/main/java/com/itheima/SpringbootRedis01Application.java
+++ springboot_redis_01/src/main/java/com/itheima/SpringbootRedis01Application.java
@@ -1,4 +1,4 @@
-package com.itheima.springboot_redis_01;
+package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ springboot_redis_01/src/test/java/com/itheima/SpringbootRedis01ApplicationTests.java
@@ -0,0 +1,30 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+class SpringbootRedis01ApplicationTests {
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    @Test
+    void contextLoads() {
+
+        ValueOperations<String, String> ssvo = redisTemplate.opsForValue();
+
+        ssvo.set("name01","alicization");
+
+        String name02 = ssvo.get("name02");
+
+        System.out.println(name02);
+
+
+    }
+
+}
