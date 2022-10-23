332af6a45514a3045fd106aa6e6e50cf70a1a112
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-24 07:29:42 +0800

    AM

--- springboot_redis_01/src/test/java/com/itheima/SpringbootRedis01ApplicationTests.java
+++ springboot_redis_01/src/test/java/com/itheima/SpringbootRedis01ApplicationTests.java
@@ -9,12 +9,28 @@ import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
 
 import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 class SpringbootRedis01ApplicationTests {
 
     @Autowired
     private RedisTemplate<String,String> redisTemplate;
+
+
+    @Test
+    void testSet(){
+
+        SetOperations<String, String> ssso = redisTemplate.opsForSet();
+
+        ssso.add("s1","ryo","EGOIST","chelly","chelly");
+
+        Set<String> l1 = ssso.members("l1");
+
+        l1.forEach(System.out::println);
+
+
+    }
 
     @Test
     void testList(){
