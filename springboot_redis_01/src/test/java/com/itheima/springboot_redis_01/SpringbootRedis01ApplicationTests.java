28fee6feeaeb32d3f626fb2d5af652782337db45
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-23 08:02:10 +0800

    AM

---
+++ springboot_redis_01/src/test/java/com/itheima/springboot_redis_01/SpringbootRedis01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_redis_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedis01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
