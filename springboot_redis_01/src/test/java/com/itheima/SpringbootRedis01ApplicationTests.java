8df16280ab153a7dc83faa5f431dc172ad6e150a
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-24 07:27:49 +0800

    AM

--- springboot_redis_01/src/test/java/com/itheima/SpringbootRedis01ApplicationTests.java
+++ springboot_redis_01/src/test/java/com/itheima/SpringbootRedis01ApplicationTests.java
@@ -3,9 +3,12 @@ package com.itheima;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringbootRedis01ApplicationTests {
@@ -14,7 +17,22 @@ class SpringbootRedis01ApplicationTests {
     private RedisTemplate<String,String> redisTemplate;
 
     @Test
-    void contextLoads() {
+    void testList(){
+
+        ListOperations<String, String> sslo = redisTemplate.opsForList();
+
+        sslo.leftPushAll("l1","爱丽丝滋贝鲁库萨提","Eugeo","kirito","asuna","诗乃");
+
+        List<String> l1 = sslo.range("l1", 0, -1);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        l1.forEach(System.out::println);
+
+    }
+
+    @Test
+    void testValue() {
 
         ValueOperations<String, String> ssvo = redisTemplate.opsForValue();
 
